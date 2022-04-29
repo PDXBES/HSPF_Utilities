07d5770892d750f14421d9c7a5eeebe3981f8deb
sggBES <30871937+sggBES@users.noreply.github.com>
2022-04-29 16:57:46 -0700

    Updated routines to write unrouted flow for the PAC to looping through multiple rain gage and HRU data sets and write an ouput file for each set of data. Reverted to an older ouput file format.

--- pac/unrouted_runoff_for_pac.py
+++ pac/unrouted_runoff_for_pac.py
@@ -2,18 +2,21 @@ from wdmtoolbox import wdmtoolbox
 import pandas as pd
 
 start_date_time = "10/1/1977"
-stop_date_time = "10/1/2020"
-rg_dsn = 10
+stop_date_time = "10/1/2021"
+rgs = [3, 4, 10, 14, 153]
 evap_dsn = 1
 interval_in_minutes = 5
+area_in_sqft = 10000
+
+hru_area_in_acres = 1
+number_of_intervals_after_storm_event = 1440
+
 flow_units = "cfs"
 area_units = "acre"
-hru_area_in_acres = 1
+
 precip_units = "in/{} min".format(interval_in_minutes)
 evap_units = "in/{} min".format(interval_in_minutes)
-number_of_intervals_after_storm_event = 1440
-description = "RG{}".format(rg_dsn) + " " + "HRU"
-area_in_acres = 1
+area_in_acres = area_in_sqft / 43560
 
 suro_impervious_dsns = {'Building': (1101, area_in_acres), 'Road/Pkg Flat': (1102, area_in_acres), 'Road/Pkg Moderate': (1103, area_in_acres), 'Road/Pkg Stp': (1104, area_in_acres), 'Road/Pkg VStp': (1105, area_in_acres)}
 suro_ifwo_predeveloped_pervious_base_dsn = {
@@ -28,16 +31,31 @@ suro_ifwo_predeveloped_pervious_base_dsn = {
                                             'Saturated Forest Steep': (21, area_in_acres),
                                             }
 
-suro_impervious_dsns = {'Post': (1101, area_in_acres)}
-suro_ifwo_predeveloped_pervious_base_dsn = {'Pre': (11, area_in_acres)}
+#suro_impervious_dicts = [{'Post': (1101, area_in_acres, 'Building')}]
+suro_ifwo_predeveloped_pervious_dicts = [{'Pre': (10, area_in_acres, 'Forest Flat C', 'Tryon W')},
+                                         {'Pre': (11, area_in_acres, 'Forest Mod C', 'Tryon W')},
+                                         {'Pre': (12, area_in_acres, 'Forest Stp C', 'Tryon W')},
 
-precip_dsn = [rg_dsn]
+                                         {'Pre': (110, area_in_acres, 'Forest Flat C', 'Tryon S')},
+                                         {'Pre': (111, area_in_acres, 'Forest Mod C', 'Tryon S')},
+                                         {'Pre': (112, area_in_acres, 'Forest Stp C', 'Tryon S')},
 
-output_file_name = "rg{}_impervious_hru_output_wy_1977_to_wy_2020".format(rg_dsn)
-met_data_wdm_file_name = r"Met5min.wdm"
-met_data_wdm_file = r"C:\Temp\pac\sim" + "\\" + "Pac_RG{}".format(rg_dsn) + "\\" + met_data_wdm_file_name
-unrouted_flow_data_wdm_file = r"C:\Temp\pac\sim" + "\\" + "Pac_RG{}".format(rg_dsn) + "\\" + "hru" + str(rg_dsn) + ".wdm"
-output_csv_file = r"C:\Temp\pac" + "\\" + "" + output_file_name + ".txt"
+                                         {'Pre': (410, area_in_acres, 'Forest Flat C', 'Tryon NE')},
+                                         {'Pre': (411, area_in_acres, 'Forest Mod C', 'Tryon NE')},
+                                         {'Pre': (412, area_in_acres, 'Forest Stp C', 'Tryon NE')},
+
+                                         {'Pre': (210, area_in_acres, 'Forest Flat C', 'Western Washington Regional Parameters')},
+                                         {'Pre': (211, area_in_acres, 'Forest Mod C', 'Western Washington Regional Parameters')},
+                                         {'Pre': (212, area_in_acres, 'Forest Stp C', 'Western Washington Regional Parameters')},
+
+                                         # {'Pre': (311, area_in_acres, 'Forest Flat C', 'Trust')},
+                                         # {'Pre': (312, area_in_acres, 'Forest Mod C', 'Trust')},
+                                         # {'Pre': (313, area_in_acres, 'Forest Stp C', 'Trust')}
+                                         ]
+suro_impervious_dicts = len(suro_ifwo_predeveloped_pervious_dicts)*[{'Post': (2001, area_in_acres, 'Building')}]
+
+
+
 
 
 def filter_df_based_on_start_and_stop_dates(df, start_date_time, stop_date_time):
@@ -46,7 +64,7 @@ def filter_df_based_on_start_and_stop_dates(df, start_date_time, stop_date_time)
     return df
 
 
-def extract_dsn_and_resample(dsn, wdm_file, output_timestep_in_minutes=None):
+def _extract_dsn_and_resample(dsn, wdm_file, output_timestep_in_minutes=None):
     flow = wdmtoolbox.extract(wdm_file, dsn).fillna(value=0)
     if output_timestep_in_minutes is not None:
         flow = flow.resample(str(output_timestep_in_minutes) + 'T').mean()
@@ -59,7 +77,7 @@ def create_hru_surface_flow_dataframe(wdm_file, surface_flow_dsns, start_date_ti
     hru_surface_flows = []
     for name, dsn_and_area in zip(dsn_names, dsns):
         dsn = dsn_and_area[0]
-        flow = extract_dsn_and_resample(dsn, wdm_file)
+        flow = _extract_dsn_and_resample(dsn, wdm_file)
         flow = filter_df_based_on_start_and_stop_dates(flow, start_date_time, stop_date_time)
         flow = flow.rename(columns={flow.columns[0]: name})
         flow[name] = flow[name] * area_in_acres/hru_area_in_acres
@@ -75,12 +93,12 @@ def create_hru_pervious_flow_dataframe(wdm_file, suro_ifwo_predeveloped_pervious
     for name, dsn_and_area in zip(dsn_names, dsns):
         dsn = dsn_and_area[0]
         suro_dsn = dsn + 1000
-        suro_flow = extract_dsn_and_resample(suro_dsn, wdm_file)
+        suro_flow = _extract_dsn_and_resample(suro_dsn, wdm_file)
         suro_flow = filter_df_based_on_start_and_stop_dates(suro_flow, start_date_time, stop_date_time)
         suro_flow = suro_flow.rename(columns={suro_flow.columns[0]: name})
 
-        ifwo_dsn = dsn + 1200
-        ifwo_flow = extract_dsn_and_resample(ifwo_dsn, wdm_file)
+        ifwo_dsn = dsn + 3000
+        ifwo_flow = _extract_dsn_and_resample(ifwo_dsn, wdm_file)
         ifwo_flow = filter_df_based_on_start_and_stop_dates(ifwo_flow, start_date_time, stop_date_time)
         ifwo_flow = ifwo_flow.rename(columns={ifwo_flow.columns[0]: name})
         pervious_flow = suro_flow[name].add(ifwo_flow[name]).to_frame()
@@ -92,40 +110,55 @@ def create_hru_pervious_flow_dataframe(wdm_file, suro_ifwo_predeveloped_pervious
     return hru_surface_flow_df
 
 
-precip = extract_dsn_and_resample(rg_dsn, met_data_wdm_file, output_timestep_in_minutes=5)
-precip = filter_df_based_on_start_and_stop_dates(precip, start_date_time, stop_date_time)
-precip = precip.rename(columns={precip.columns[0]: 'Precip'})
+for rg_dsn in rgs:
+    for suro_impervious_dict, suro_ifwo_predeveloped_pervious_dict in zip(suro_impervious_dicts, suro_ifwo_predeveloped_pervious_dicts):
+        post_column_name = list(suro_impervious_dict.keys())[0]
+        pre_column_name = list(suro_ifwo_predeveloped_pervious_dict.keys())[0]
+        post_dsn = list(suro_impervious_dict.values())[0][0]
+        pre_dsn = list(suro_ifwo_predeveloped_pervious_dict.values())[0][0]
+        pre_description = list(suro_ifwo_predeveloped_pervious_dict.values())[0][2] + " " + list(suro_ifwo_predeveloped_pervious_dict.values())[0][3] + " " + str(pre_dsn)
+        post_description = list(suro_impervious_dict.values())[0][2] + " " + str(post_dsn)
+        sim = r"\\BESFile1\ASM_Projects\PACHSPFRunoff\Models\TryonParameters\sim"
+        simulation_folder = sim + "\\" + "TryonCreekParameters_EXISTING_RG{}".format(rg_dsn)
+        output_file_name = "{}sqft_Pre_{}_Post_{}_RG{}_output_wy_1978_to_wy_2021".format(area_in_sqft, pre_description, post_description, rg_dsn)
+        description = "Area in sq ft:" + str(area_in_sqft) + str() + " Pre:" + pre_description + " Post:" + post_description + " RG{}".format(rg_dsn)
+        met_data_wdm_file_name = r"Met5min.wdm"
+        evap_data_wdm_file_name = r"Evap5min.wdm"
+        met_data_wdm_file = simulation_folder + "\\" + met_data_wdm_file_name
+        evap_data_wdm_file = sim + "\\" + evap_data_wdm_file_name
+        unrouted_flow_data_wdm_file = simulation_folder + "\\" + "hru" + str(rg_dsn) + ".wdm"
+        output_csv_file = r"\\BESFile1\ASM_Projects\PACHSPFRunoff\Models\TryonParameters\sim\runoff" + "\\" + "" + output_file_name + ".txt"
 
-evap = extract_dsn_and_resample(evap_dsn, met_data_wdm_file, output_timestep_in_minutes=5)
-evap = filter_df_based_on_start_and_stop_dates(evap, start_date_time, stop_date_time)
-evap = evap.rename(columns={evap.columns[0]: 'Evap'})
+        precip = _extract_dsn_and_resample(rg_dsn, met_data_wdm_file, output_timestep_in_minutes=5)
+        precip = filter_df_based_on_start_and_stop_dates(precip, start_date_time, stop_date_time)
+        precip = precip.rename(columns={precip.columns[0]: 'Precip'})
 
-pervious_flow_df = create_hru_pervious_flow_dataframe(unrouted_flow_data_wdm_file, suro_ifwo_predeveloped_pervious_base_dsn, start_date_time, stop_date_time)
-impervious_flow_df = create_hru_surface_flow_dataframe(unrouted_flow_data_wdm_file, suro_impervious_dsns, start_date_time, stop_date_time)
-flow_df = create_hru_surface_flow_dataframe(unrouted_flow_data_wdm_file, suro_impervious_dsns, start_date_time, stop_date_time)
-met_and_flow_df = pd.concat([precip, evap, impervious_flow_df, pervious_flow_df], axis=1)
+        evap = _extract_dsn_and_resample(evap_dsn, evap_data_wdm_file, output_timestep_in_minutes=5)
+        evap = filter_df_based_on_start_and_stop_dates(evap, start_date_time, stop_date_time)
+        evap = evap.rename(columns={evap.columns[0]: 'Evap'})
 
-start_date = pd.to_datetime(met_and_flow_df.index.values[0]).strftime("%Y-%m-%dT%H:%M:%S-08:00")
+        pervious_flow_df = create_hru_pervious_flow_dataframe(unrouted_flow_data_wdm_file, suro_ifwo_predeveloped_pervious_dict, start_date_time, stop_date_time)
+        impervious_flow_df = create_hru_surface_flow_dataframe(unrouted_flow_data_wdm_file, suro_impervious_dict, start_date_time, stop_date_time)
+        #flow_df = create_hru_surface_flow_dataframe(unrouted_flow_data_wdm_file, suro_impervious_dsns, start_date_time, stop_date_time)
+        met_and_flow_df = pd.concat([precip, evap, impervious_flow_df, pervious_flow_df], axis=1)
 
-columns = ['Precip', 'Evap'] + list(suro_impervious_dsns.keys()) + list(suro_ifwo_predeveloped_pervious_base_dsn.keys())
-with open(output_csv_file, 'w') as out:
-    out.write(description + '\r')
-    out.write(precip_units + '\r')
-    out.write(evap_units + '\r')
-    out.write(flow_units + '\r')
-    out.write(str(area_in_acres) + " " + area_units + '\r')
-    out.write(start_date + '\r')
-    out.write(str(interval_in_minutes) + '\r')
-    out.write(str(number_of_intervals_after_storm_event) + '\r')
-    columns_for_csv = ""
-    for column in columns:
-        columns_for_csv += column + ","
-    out.write(columns_for_csv[0:-1] + "\r")
-    number_of_flow_columns = len(list(suro_impervious_dsns.keys()) +
-                                 list(suro_ifwo_predeveloped_pervious_base_dsn.keys()))
+        start_date = pd.to_datetime(met_and_flow_df.index.values[0]).strftime("%Y-%m-%dT%H:%M:%S-08:00")
 
-met_and_flow_df.to_csv(output_csv_file, columns=columns, index=False, float_format='%.8f', header=False,
-                       line_terminator='\r\n', mode='a', sep=',')
+        #columns = ['Precip', 'Evap'] + [post_column_name, pre_column_name]
+        with open(output_csv_file, 'w') as out:
+            # out.write(description + '\r')
+            # out.write(precip_units + '\r')
+            # out.write(evap_units + '\r')
+            # out.write(flow_units + '\r')
+            # out.write(str(area_in_acres) + " " + area_units + '\r')
+            # out.write(start_date + '\r')
+            out.write(str(interval_in_minutes) + '\r')
+            out.write(str(number_of_intervals_after_storm_event) + '\r')
+            out.write(start_date + '\r')
+        met_and_flow_df.to_csv(output_csv_file, columns=['Post', 'Pre'], index=False, float_format='%.8f', header=False,
+                                   line_terminator='\r\n', mode='a', sep=',')
+        # met_and_flow_df.to_csv(output_csv_file, columns=['Post', 'Pre'], index=False, float_format='%.4e', header=False,
+        #                            line_terminator='\r\n', mode='a', sep=',')
 
 # with open(output_csv_file, 'w') as out:
 #     #out.write(description +"\r")
